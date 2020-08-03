7b5b5ac7f4592f29382623d961a8c54401104eb1
akirashi1998 <shikuangzheng@outlook.com>
2020-08-04 00:19:46 +0800

    200804

---
+++ leetcode/415 字符串相加.js
@@ -0,0 +1,28 @@
+var addStrings = function(num1, num2) {
+    function reverseStr(str){
+        var res = ''
+        var length = str.length
+        for(let i=0; i<length; i++){
+            res += str[length-1-i]
+        }
+        return res
+    }
+    var length1 = num1.length, length2 = num2.length
+    var mLength = Math.max(length1, length2)
+    var str1 = reverseStr(num1)
+    var str2 = reverseStr(num2)
+    var res = ''
+    var carry = 0
+    for(let i=0; i<mLength; i++){
+        let a = i<length1?parseInt(str1[i]):0
+        let b = i<length2?parseInt(str2[i]):0
+        res += ((a+b+carry)%10).toString()
+        carry = (a+b+carry)>9?1:0
+    }
+    if(carry > 0){
+        res += '1'
+    }
+    return reverseStr(res)
+};
+
+// 一样的思路，原先的写法不知道问题出在哪里
